255f8a9e5e2a6e3ca2b12f5218a2fb69a6e7d817
Fernando152006 <80235137+Fernando152006@users.noreply.github.com>
2021-03-11 18:50:38 -0300

    Add files via upload

---
+++ src/help1.js
@@ -0,0 +1,36 @@
+const help1 = (prefix) => {
+
+	return `
+╭──────────────╮
+ *COMANDOS DO BOT*
+╰──────────────╯
+ 
+➸ *${prefix}marcar*
+➸ *${prefix}marcar2*
+➸ *${prefix}marcar3*
+➸ *${prefix}loli*
+➸ *${prefix}loli1*
+➸ *${prefix}hentai*
+➸ *${prefix}dono*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}akeno*
+➸ *${prefix}meme*
+➸ *${prefix}lofi*
+➸ *${prefix}malkova*
+➸ *${prefix}canal*
+➸ *${prefix}nsfwloli*
+➸ *${prefix}reislin*
+➸ *${prefix}limpar*
+➸ *${prefix}marcar*
+➸ *${prefix}ts (texto que deseja transmitir)*
+
+════════════════════
+*DANIEL YT* 🤗
+*Digite ${prefix}dono para mais info*
+════════════════════`
+
+}
+exports.help1 = help1
+
